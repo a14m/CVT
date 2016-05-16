f98d6ef5506883ccb0e52f21fb860d5e233450d5
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-05-16 16:59:43 +0200

    STYLE: fix rubocop styling

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,7 +1,7 @@
 Clearance.configure do |config|
   config.allow_sign_up = true
   config.cookie_domain = '.katrina.dev'
-  config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
+  config.cookie_expiration = ->(_cookies) { 1.year.from_now.utc }
   config.cookie_name = 'remember_token'
   config.cookie_path = '/'
   config.routes = false
