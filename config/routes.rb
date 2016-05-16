6676f6f5572463f3a306d452c18659999a689f3e
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-05-16 16:56:29 +0200

    FEAT: add clearance routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
+  resource  :session,   controller: 'clearance/sessions',  only: [:create]
+
+  resources :users, controller: 'clearance/users', only: [:create] do
+    resource :password, controller: 'clearance/passwords', only: [:create, :edit, :update]
+  end
+
+  get    '/sign_up'  => 'clearance/users#new',        as: 'sign_up'
+  get    '/sign_in'  => 'clearance/sessions#new',     as: 'sign_in'
+  delete '/sign_out' => 'clearance/sessions#destroy', as: 'sign_out'
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
