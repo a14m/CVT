4e4aff51f4f13a01bfd1d03425021491110378cd
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-06-13 00:34:28 +0200

    FEAT: add sorcery routes and i18n errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
-  resource  :session,   controller: 'clearance/sessions',  only: [:create]
+  # resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
 
-  resources :users, controller: 'clearance/users', only: [:create] do
-    resource :password, controller: 'clearance/passwords', only: [:create, :edit, :update]
-  end
+  # resources :users, controller: 'clearance/users', only: [:create] do
+    # resource :password, controller: 'clearance/passwords', only: [:create, :edit, :update]
+  # end
 
-  get    '/sign_up'  => 'clearance/users#new',        as: 'sign_up'
-  get    '/sign_in'  => 'clearance/sessions#new',     as: 'sign_in'
-  delete '/sign_out' => 'clearance/sessions#destroy', as: 'sign_out'
+  get    '/sign_up'  => 'authentications#new_sign_up', as: 'sign_up'
+  post   '/sign_up'  => 'authentications#sign_up'
+  get    '/sign_in'  => 'authentications#new_sign_in', as: 'sign_in'
+  post   '/sign_in'  => 'authentications#sign_in'
+  delete '/sign_out' => 'authentications#destroy',     as: 'sign_out'
+
+  get '/dashboard' => 'dashboards#show'
+
+  root to: 'dashboards#index'
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
