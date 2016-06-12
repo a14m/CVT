bab53ba9d5878b25dad87ec45ee27f048e1fc2fd
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-06-13 00:30:41 +0200

    FEAT: setup sorcery login and not_authenticated route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 # Base Application Controller Class
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def not_authenticated
+    redirect_to sign_in_path, notice: I18n.t('authentications.login_required')
+  end
 end
