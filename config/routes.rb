23ef9507376e7e762ddd287993b87b002a81d95d
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-06-20 23:22:03 +0200

    FEAT: clean up router and add new password reset routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
-  # resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
-
-  # resources :users, controller: 'clearance/users', only: [:create] do
-    # resource :password, controller: 'clearance/passwords', only: [:create, :edit, :update]
-  # end
-
+  # Authentications
   get    '/sign_up'  => 'authentications#new_sign_up', as: 'sign_up'
-  post   '/sign_up'  => 'authentications#sign_up'
   get    '/sign_in'  => 'authentications#new_sign_in', as: 'sign_in'
-  post   '/sign_in'  => 'authentications#sign_in'
-  delete '/sign_out' => 'authentications#destroy',     as: 'sign_out'
 
+  post   '/sign_up'  => 'authentications#sign_up'
+  post   '/sign_in'  => 'authentications#sign_in'
+  delete '/sign_out' => 'authentications#destroy', as: 'sign_out'
+
+  # Passwords
+  get '/reset_password'  => 'passwords#new_reset_password_instructions', as: 'reset_password_instructions'
+  get '/:token/password' => 'passwords#new_reset_password', as: 'reset_password'
+
+  post '/reset_password'  => 'passwords#reset_password_instructions'
+  post '/:token/password' => 'passwords#reset_password'
+
+  # Dashboard
   get '/dashboard' => 'dashboards#show'
 
   root to: 'dashboards#index'
