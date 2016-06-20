6a7cda4de51d34516713aa44dda2b288179fa36c
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-06-21 00:13:00 +0200

    FIX: PasswordsMailer reset password instructions

--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -1,13 +1,12 @@
 # Authentication Mailer class
-class AuthenticationMailer < ApplicationMailer
+class PasswordsMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
-  #   en.authentication_mailer.reset_password_email.subject
+  #   en.passwords_mailer.reset_password_email.subject
   #
   def reset_password_email(user)
     @user = User.find user.id
-    @url  = api_password_reset_url(@user.reset_password_token)
     mail(to: user.email)
   end
 end
