06a5a92cc9353850947bd73a8d8237fc3276d8b2
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-07-23 18:04:19 +0200

    FEAT: add the migration for the torrent model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606011053) do
+ActiveRecord::Schema.define(version: 20160723150458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
+
+  create_table "torrents", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string   "name"
+    t.integer  "transmission_id"
+    t.integer  "size"
+    t.string   "checksum"
+    t.string   "path"
+    t.string   "file"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["user_id"], name: "index_torrents_on_user_id", using: :btree
+  end
 
   create_table "users", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.string   "email",                           null: false
