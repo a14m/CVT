ae27c92d156f995ce544044664096041cdbd818b
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-08-14 19:24:34 +0200

    REFACTOR: migration to use rails 5

--- db/migrate/20160814153000_add_attachment_torrent_to_torrents.rb
+++ db/migrate/20160814153000_add_attachment_torrent_to_torrents.rb
@@ -1,11 +1,7 @@
-class AddAttachmentTorrentToTorrents < ActiveRecord::Migration
-  def self.up
+class AddAttachmentTorrentToTorrents < ActiveRecord::Migration[5.0]
+  def change
     change_table :torrents do |t|
       t.attachment :torrent
     end
-  end
-
-  def self.down
-    remove_attachment :torrents, :torrent
   end
 end
