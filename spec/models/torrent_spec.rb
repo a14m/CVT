6c23a704f204b9bde712033e369e2adb32e9adf8
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-08-14 19:14:01 +0200

    TEST: add torrent attachment testing

--- spec/models/torrent_spec.rb
+++ spec/models/torrent_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe Torrent, type: :model do
     it { is_expected.to validate_presence_of(:size) }
     it { is_expected.to validate_presence_of(:checksum) }
     it { is_expected.to validate_presence_of(:path) }
-    it { is_expected.to validate_presence_of(:file) }
+    it { should have_attached_file(:torrent) }
+    it { should validate_attachment_presence(:torrent) }
+    it do
+      should validate_attachment_content_type(:torrent)
+        .allowing('application/x-bittorrent')
+    end
   end
 end
