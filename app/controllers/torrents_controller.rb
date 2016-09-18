b70fbbae1a1c4c23676de64626996003e581ac5f
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-09-18 04:19:31 +0200

    FIX: pass the correct parameter to SO

--- app/controllers/torrents_controller.rb
+++ app/controllers/torrents_controller.rb
@@ -2,9 +2,13 @@
 class TorrentsController < ApplicationController
   # POST /torrents
   def create
-    TorrentCreation.call(file: request.env['rack.input'])
+    TorrentCreation.call(file: torrent_params[:file])
     render status: 201, json: { message: I18n.t('torrents.file_added') }
   rescue TorrentCreationError => e
     render status: 403, json: { message: I18n.t(e.message) }
+  end
+
+  def torrent_params
+    params.require(:torrent).permit(:file)
   end
 end
