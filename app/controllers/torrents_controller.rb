b845c6f48be71aa8ab55940f7305b8f6da213649
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-09-25 22:55:08 +0200

    FEAT: handle different validation errors
    FEAT: handle service errors when transmission fails or stops with 500

--- app/controllers/torrents_controller.rb
+++ app/controllers/torrents_controller.rb
@@ -8,6 +8,11 @@ class TorrentsController < ApplicationController
     render status: 400, json: { message: I18n.t(e.message) }
   rescue TorrentCreationError => e
     render status: 403, json: { message: I18n.t(e.message) }
+  rescue ActiveRecord::RecordInvalid => e
+    logger.warn e.message
+    render status: 422, json: { message: I18n.t('torrents.duplicate_file') }
+  rescue ApplicationError
+    render status: 500, json: { message: I18n.t('torrents.service_stopped') }
   end
 
   def torrent_params
