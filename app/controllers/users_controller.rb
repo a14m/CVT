acfab5f650e0163526ad92dea0574e2a2ba77658
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-10-21 18:35:16 +0200

    FEAT: implement unsubscribe action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 # User controller
 class UsersController < ApplicationController
+  # GET /user
   def show
     @user = current_user.decorate
+  end
+
+  # DELETE /user/unsubscribe
+  def unsubscribe
+    if current_user.subscription_id?
+      current_user.subscription_id = nil
+      current_user.save!
+      flash[:success] = I18n.t('subscription.unsubscribed')
+    else
+      flash[:notice] = I18n.t('subscription.already_unsubscribed',
+        expires_at: current_user.decorate.expires_at)
+    end
+    redirect_to :user
   end
 end
