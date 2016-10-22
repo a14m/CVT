3dd785cfa34aeed48f4efe5dc67f0840198632d5
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-10-22 14:07:36 +0200

    REFACTOR: subscribe/unsubscribe actions in the UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,35 @@ class UsersController < ApplicationController
     @user = current_user.decorate
   end
 
+  # POST /user/subscribe
+  def subscribe
+    fail SubscriptionError unless UserPolicy.new(current_user).can_subscribe?
+    SubscriptionCreation.new.call(
+      user: current_user,
+      token: params[:stripeToken]
+    )
+    flash[:success] = I18n.t('subscription.subscribed',
+      expires_at: current_user.decorate.expires_at)
+  rescue SubscriptionError
+    flash[:notice] = I18n.t('subscription.already_subscribed',
+      expires_at: current_user.decorate.expires_at)
+  rescue Stripe::StripeError => e
+    flash[:error] = e.message
+  ensure
+    redirect_to :user
+  end
+
   # DELETE /user/unsubscribe
   def unsubscribe
-    if current_user.subscription_id?
-      current_user.subscription_id = nil
-      current_user.save!
-      flash[:success] = I18n.t('subscription.unsubscribed')
-    else
-      flash[:notice] = I18n.t('subscription.already_unsubscribed',
-        expires_at: current_user.decorate.expires_at)
-    end
+    fail SubscriptionError unless UserPolicy.new(current_user).can_unsubscribe?
+    SubscriptionCancellation.new.call(user: current_user)
+    flash[:success] = I18n.t('subscription.unsubscribed')
+  rescue SubscriptionError
+    flash[:notice] = I18n.t('subscription.already_unsubscribed',
+      expires_at: current_user.decorate.expires_at)
+  rescue Stripe::StripeError => e
+    flash[:error] = e.message
+  ensure
     redirect_to :user
   end
 end
