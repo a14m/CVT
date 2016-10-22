3164759ff5aba4e9aefb57598d6d80b93a303b81
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-10-22 14:55:10 +0200

    FIX: UserCreation not to expose stripe errors

--- app/services/user_creation.rb
+++ app/services/user_creation.rb
@@ -1,15 +1,23 @@
 # Service Object for User/Stripe customer object creation
 class UserCreation
   def call(email:, password:)
-    stripe_customer = Stripe::Customer.create(
-      email: email
-    )
-
-    User.create!(
+    user = User.create!(
       email: email,
       password: password,
-      expires_at: 3.days.from_now,
-      stripe_id: stripe_customer.id
+      expires_at: 3.days.from_now
     )
+
+    stripe_customer = Stripe::Customer.create(
+      email: user.email
+    )
+
+    # Update the user with Stripe Information
+    user.stripe_id = stripe_customer.id
+    user.save!
+    user
+  rescue Stripe::StripeError => e
+    Rails.logger.warn e.message
+    user.destroy!
+    raise e
   end
 end
